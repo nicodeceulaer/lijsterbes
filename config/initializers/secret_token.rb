63da8b51a0394572130ea22c6faa7d920a4840bb
Nico De Ceulaer <nico.de.ceulaer@gmail.com>
2013-05-22 22:05:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lijsterbes::Application.config.secret_key_base = 'b2cfda763ec28ff82f7459d7c547f84d8f05c785218d908a7ed7fcb2e3f759a93e00e9ec3953b602e2edb3689434f0634f5b7cc7ca1ad12de33cb65af6cad733'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lijsterbes::Application.config.session_store :cookie_store, key: '_lijsterbes_session'
